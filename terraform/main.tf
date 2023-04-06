730b03c73f2a1d35b6d536f05fc10ed668d30515
mrsvllmr <96266054+mrsvllmr@users.noreply.github.com>
2023-04-06 18:55:52 +0200

    deleted gcsfs version in requirements.txt due to dependency issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,24 +26,6 @@ resource "google_compute_instance" "instance" {
     sshKeys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
   }
 
-  # Use the service account to authenticate the GC cli
-  #metadata_startup_script = <<-SCRIPT
-  #  # Create the directory for the service account key file
-  #  mkdir -p /home/mrsvllmr/.gc && touch /home/mrsvllmr/.gc/sa_key_file.json
-  #  
-  #  # Write the service account key file
-  #  echo "${google_service_account_key.de-zoomcamp-2023-project-sa-key}" > /home/mrsvllmr/.gc/sa_key_file.json
-  #  
-  #  # make files visible
-  #  ls -a
-  #
-  #  # Set the GOOGLE_APPLICATION_CREDENTIALS environment variable
-  #  export GOOGLE_APPLICATION_CREDENTIALS=/home/mrsvllmr/.gc/sa_key_file.json
-  #  
-  #  # Authenticate the GC cli
-  #  gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
-  #SCRIPT
-
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
@@ -52,50 +34,8 @@ resource "google_compute_instance" "instance" {
       private_key = file(var.gce_ssh_priv_key_file)
     }
     inline = [
-      # "sudo apt-add-repository -r ppa:gnome3-team/gnome3",
-      # "sudo apt-add-repository -r ppa:philip.scott/spice-up-daily",
-      # "sudo apt-get install -y docker.io",
-      # "sudo groupadd docker",
-      # "sudo gpasswd -a $USER docker",
-      # "sudo service docker restart",
-      # "sudo systemctl start docker",
-      # "sudo systemctl enable docker",
-      # "cd ~",
-      # "mkdir bin",
-      # "cd ~/bin",
-      # "wget https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -O docker-compose",
-      # # "sudo apt-get install -y docker-compose"
-      # "chmod +x docker-compose",
-      # "docker-compose up -d"
-
-      # "sudo apt-get update",
-      # "sudo apt-get install git",
-      # # "git clone https://github.com/mrsvllmr/de_zoomcamp_2023_project.git",
-      # "git clone https://mrsvllmr:${file(var.github_pat)}@github.com/mrsvllmr/de_zoomcamp_2023_project.git",
-      # "sudo apt-get install wget",
-      # "wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh",
-      # "cd /home/mrsvllmr/de_zoomcamp_2023_project",
-      # "conda create -n conda_venv",
-      # "conda activate conda-env",
-      # "conda install pip",
-      # "pip install -r requirements.txt"
-
-      # "git clone https://mrsvllmr:${file(var.github_pat)}@github.com/mrsvllmr/de_zoomcamp_2023_project.git",
-      # "sudo apt-get install wget",
-      # "wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh",
-      # "bash Anaconda3-2022.10-Linux-x86_64.sh -b -p /home/mrsvllmr/anaconda3",
-      # "export PATH=/home/mrsvllmr/anaconda3/bin:$PATH",
-      # "source /home/mrsvllmr/anaconda3/etc/profile.d/conda.sh",
-      # "conda create -y -n conda_venv python=3.9",
-      # "conda deactivate",
-      # "source /home/mrsvllmr/anaconda3/bin/activate conda_venv",
-      # "cd /home/mrsvllmr/de_zoomcamp_2023_project",
-      # "pip install -r requirements.txt"
-
-      #"git clone https://mrsvllmr:${file(var.github_pat)}@github.com/mrsvllmr/de_zoomcamp_2023_project.git", # temporarily used when repo was private
       "git clone https://github.com/mrsvllmr/de_zoomcamp_2023_project.git",
       "sudo apt-get install wget",
-      #"sudo apt-get install git",
       "wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh",
       "bash Anaconda3-2022.10-Linux-x86_64.sh -b -p /home/mrsvllmr/anaconda3",
       "export PATH=/home/mrsvllmr/anaconda3/bin:$PATH",
@@ -108,25 +48,3 @@ resource "google_compute_instance" "instance" {
     ]
   }
 }
-
-################################################################################################################################################
-# Docker compose visibility
-################################################################################################################################################
-# make Docker Compose visible from every directory
-# resource "null_resource" "add_bin_to_path" {
-#   provisioner "file" {
-#     content = "export PATH=\"${HOME}/bin:${PATH}\""
-#     destination = "~/.bashrc"
-#     connection {
-#       type     = "ssh"
-#       user     = var.gce_ssh_user
-#       host     = var.instance
-#       private_key = file(var.gce_ssh_priv_key_file)
-#     }
-#   }
-# }
-
-# resource "local_file" "de-zoomcamp-2023-project-sa-key-json" {
-#   filename = "/home/mrsvllmr/.gc/sa_key_file.json"
-#   content  = base64decode(google_service_account_key.de-zoomcamp-2023-project-sa-key.private_key)
-# }
